7fcb31d09f843a7061a32c38ff0bd4c394f97588
Santiago Zanella-Beguelin <santiago@microsoft.com>
2019-05-09 12:12:58 +0100

    Generate corpus if not already done

--- fuzz.sh
+++ fuzz.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -eu
-cd /src/cryptofuzz && ./cryptofuzz -dict=cryptofuzz-dict.txt CORPUS -print_pcs=1 |& tee -a log
+cd /src/cryptofuzz
+
+if [ ! -d "CORPUS" ]; then
+  mkdir corpus && ./generate_corpus
+fi
+
+./cryptofuzz -dict=cryptofuzz-dict.txt CORPUS -print_pcs=1 |& tee -a log
